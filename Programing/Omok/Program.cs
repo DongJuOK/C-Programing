c77ddf7394c50705e0b7fe009ae0dbc088d0a16b
DongJuOK <kcary0907@gmail.com>
2025-12-30 12:52:14 +0900

    (2025.12.30) 오목 실습

--- Programing/Omok/Program.cs
+++ Programing/Omok/Program.cs
@@ -1,136 +1,162 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Omok
 {
-
     public class Omok
     {
         private int[,] m_seat;
-        //오목 판 사이즈
         private int m_size;
-
-
-        //현재 내가 둘수 있는 좌표 X값
+        public int Size
+        {
+            get { return m_size; }
+        }
         private int m_curX;
         public int CurX
         {
             get { return m_curX; }
-            set
-            {
+            set 
+            { 
                 m_curX = value;
+                
                 if (m_curX < 0)
+                {
                     m_curX = 0;
-                else if (m_curX > m_size - 1)
+                }
+                else if (m_curX >= m_size - 1)
+                {
                     m_curX = m_size - 1;
+                }
             }
         }
-
-        //현재 내가 둘수 있는 좌표 Y값
         private int m_curY;
         public int CurY
         {
             get { return m_curY; }
             set
-            {
+            { 
                 m_curY = value;
+
                 if (m_curY < 0)
+                {
                     m_curY = 0;
-                else if (m_curY > m_size - 1)
+                }
+                else if (m_curY >= m_size - 1)
+                {
                     m_curY = m_size - 1;
+                }
             }
         }
 
         private bool m_isBlackTurn;
+        private bool m_isPlaying;
+        public bool IsPlaying
+        {
+            get { return m_isPlaying; }
+        }
 
-        public bool IsPlaing { get; set; }
-
-
-        /// <summary>
-        /// 오목의 생성자
-        /// </summary>
-        /// <param name="_size"> 오목판 사이즈 </param>
         public Omok(int _size = 19)
         {
-            m_size = _size;
-            CurX = m_size / 2;
-            CurY = m_size / 2;
-            m_seat = new int[m_size, m_size];
-
-            for (int y = 0; y < m_size; y++)
+            if (_size < 7)
             {
-                for (int x = 0; x < m_size; x++)
+                _size = 7;
+            }
+            else if (_size > 19)
+            {
+                _size = 19;
+            }
+
+            m_seat = new int[_size, _size];
+            m_size = _size;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
                 {
                     m_seat[y, x] = 0;
                 }
             }
 
+            CurX = _size / 2;
+            CurY = _size / 2;
+
             m_isBlackTurn = true;
-            IsPlaing = true;
+            m_isPlaying = true;
         }
 
-        /// <summary>
-        /// 현재 오목판 상황을 보여주는 메서드
-        /// </summary>
-        /// <param name="_seat"> 만들어진 오목판의 시트 </param>
-        public void ShowOmok()
+        public void ShowScreen()
         {
             Console.Clear();
+
             for (int y = 0; y < m_seat.GetLength(0); y++)
             {
                 for (int x = 0; x < m_seat.GetLength(1); x++)
                 {
-                    if (CurX == x && CurY == y)
+                    if (y == CurY && x == CurX)
+                    {
                         Console.Write("C");
+                    }
                     else if (m_seat[y, x] == 1)
+                    {
                         Console.Write("B");
+                    }
                     else if (m_seat[y, x] == 2)
+                    {
                         Console.Write("W");
-                    else if (m_seat[y, x] == 0)
+                    }
+                    else
+                    {
                         Console.Write("'");
+                    }
                     Console.Write(" ");
                 }
                 Console.WriteLine();
             }
 
             if (m_isBlackTurn)
+            {
                 Console.WriteLine("흑돌 턴");
+            }
             else
+            {
                 Console.WriteLine("백돌 턴");
+            }
         }
 
-        /// <summary>
-        /// 오목 조작 관련 메서드
-        /// </summary>
-        public void InputKey()
+        public void Input()
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            ConsoleKeyInfo key = Console.ReadKey(true);
 
-
-            switch (keyInfo.Key)
+            switch (key.Key)
             {
                 case ConsoleKey.UpArrow:
                 case ConsoleKey.W:
                     CurY--;
                     break;
+
                 case ConsoleKey.DownArrow:
                 case ConsoleKey.S:
                     CurY++;
                     break;
+
                 case ConsoleKey.RightArrow:
                 case ConsoleKey.D:
                     CurX++;
                     break;
+
                 case ConsoleKey.LeftArrow:
                 case ConsoleKey.A:
                     CurX--;
                     break;
 
                 case ConsoleKey.Spacebar:
-                    PutStone();
+                    InputStone();
                     break;
 
                 default:
@@ -138,188 +164,219 @@ namespace Omok
             }
         }
 
-        /// <summary>
-        /// 돌을 두는 메서드
-        /// </summary>
-        private void PutStone()
+        public void InputStone()
         {
-            int x = m_curX;
-            int y = m_curY;
-
-            if (m_seat[y, x] == 0)
+            if (m_seat[CurY, CurX] == 0)
             {
-                //if (m_isBlackTurn)
-                //    m_seat[y, x] = 1;
-                //else
-                //    m_seat[y, x] = 2;
-                m_seat[y, x] = m_isBlackTurn ? 1 : 2;
-                if (!CheckFiveStone())
+                if (m_isBlackTurn)
+                {
+                    m_seat[CurY, CurX] = 1;
+                }
+                else
+                {
+                    m_seat[CurY, CurX] = 2;
+                }
+
+                if (!RuleCheck())
+                {
                     m_isBlackTurn = !m_isBlackTurn;
+                }
+                else
+                {
+                    m_isPlaying = false;
+                    Console.WriteLine("승리!");
+                }
             }
         }
 
-        /// <summary>
-        /// 돌이 5개 연결되는지 체크하는 메서드
-        /// </summary>
-        /// <returns></returns>
-        private bool CheckFiveStone()
+        public bool RuleCheck()
         {
-            int checkNum = m_isBlackTurn == true ? 1 : 2;
-            //if (m_isBlackTurn)
-            //    checkNum = 1;
-            //else
-            //    checkNum = 2;
+            int count = 1;
             int x = CurX;
             int y = CurY;
 
-            //m_seat[y, x] 내가 방금 돌을 둔 좌표
+            int checkNum = m_seat[CurY, CurX];
 
-            //가로 체크 계산
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                int count = 0;
-                for (int j = 0; j < 5; j++)
+                int xDt = CurX - i;
+
+                if (xDt < 0)
                 {
-                    //돌이 다섯개 연결됬을때
-                    int temp = i + j - 4;
-                    if (x - temp < 0 || x - temp >= m_size)
-                        continue;
+                    break;
+                }
 
-                    if (m_seat[y, x - temp] == checkNum)
-                        count++;
-                    else
-                        break;
-
-                    if (count == 5)
-                    {
-                        IsPlaing = false;
-                        return true;
-                    }
+                if (m_seat[y, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
                 }
             }
 
-            //세로 체크 계산
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                int count = 0;
-                for (int j = 0; j < 5; j++)
+                int xDt = CurX + i;
+
+                if (xDt >= m_size)
                 {
-                    int temp = i + j - 4;
-                    if (y - temp < 0 || y - temp >= m_size)
-                        continue;
+                    break;
+                }
 
-                    if (m_seat[y - temp, x] == checkNum)
-                        count++;
-                    else
-                        break;
-
-                    if (count == 5)
-                    {
-                        IsPlaing = false;
-                        return true;
-                    }
+                if (m_seat[y, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
                 }
             }
 
-            //대각선 오름차순 계산
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                int count = 0;
-                for (int j = 0; j < 5; j++)
+                int yDt = CurY - i;
+
+                if (yDt < 0)
                 {
-                    int temp = i + j - 4;
-                    if (y - temp < 0 || y - temp >= m_size ||
-                        x - temp < 0 || x - temp >= m_size)
-                        continue;
+                    break;
+                }
 
-                    if (m_seat[y - temp, x - temp] == checkNum)
-                        count++;
-                    else
-                        break;
-
-                    if (count == 5)
-                    {
-                        IsPlaing = false;
-                        return true;
-                    }
+                if (m_seat[yDt, x] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
                 }
             }
 
-            //대각선 내림차순 계산
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i <= 4; i++)
             {
-                int count = 0;
-                for (int j = 0; j < 5; j++)
+                int yDt = CurY + i;
+
+                if (yDt >= m_size)
                 {
-                    int temp = i + j - 4;
-                    if (y + temp < 0 || y + temp >= m_size ||
-                        x - temp < 0 || x - temp >= m_size)
-                        continue;
+                    break;
+                }
 
-                    if (m_seat[y + temp, x - temp] == checkNum)
-                        count++;
-                    else
-                        break;
-
-                    if (count == 5)
-                    {
-                        IsPlaing = false;
-                        return true;
-                    }
+                if (m_seat[yDt, x] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
                 }
             }
 
+            for (int i = 1; i <= 4; i++)
+            {
+                int xDt = CurX + i;
+                int yDt = CurY - i;
 
-            return false;
+                if (xDt < 0 || yDt < 0 || xDt >= m_size || yDt >= m_size)
+                {
+                    break;
+                }
+
+                if (m_seat[yDt, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 1; i <= 4; i++)
+            {
+                int xDt = CurX - i;
+                int yDt = CurY + i;
+
+                if (xDt < 0 || yDt < 0 || xDt >= m_size || yDt >= m_size)
+                {
+                    break;
+                }
+
+                if (m_seat[yDt, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 1; i <= 4; i++)
+            {
+                int xDt = CurX - i;
+                int yDt = CurY - i;
+
+                if (xDt < 0 || yDt < 0 || xDt >= m_size || yDt >= m_size)
+                {
+                    break;
+                }
+
+                if (m_seat[yDt, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 1; i <= 4; i++)
+            {
+                int xDt = CurX + i;
+                int yDt = CurY + i;
+
+                if (xDt < 0 || yDt < 0 || xDt >= m_size || yDt >= m_size)
+                {
+                    break;
+                }
+
+                if (m_seat[yDt, xDt] == checkNum)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count >= 5;
         }
-
-
-        public bool GetIsBlackTurn()
-        {
-            return m_isBlackTurn;
-        }
-
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Omok omok = new Omok();
+            Omok omok = new Omok(13);
 
-            while (omok.IsPlaing)
+            while(omok.IsPlaying)
             {
-                omok.ShowOmok();
-                omok.InputKey();
+                omok.ShowScreen();
+                omok.Input();
             }
-
-            if (omok.GetIsBlackTurn())
-                Console.WriteLine("흑돌 승리");
-            else
-                Console.WriteLine("백돌 승리");
-            Console.ReadLine();
         }
-
-
-
     }
 }
 
-
-// 오목을 만들것입니다.
-// 이제 우리 뭐함?
-// 1. 흑돌과 백돌이 필요하다.
-//    - 각각 한번씩 돌을 놔야한다.
-//    - 완
-// 2. 오목판 - (좌표) 
-//    - 완
-// 3. 입력받기 - 인풋처리
-//    - 완
-// 4. 내가 어디 둘것인지 대한 좌표 - 현재 나의 좌표
-//    - 완
-//  
-// 5. 알고리즘
-//    - 많은데이터를 저장하고 관리하는게 알고리즘
-// 룰정리 - 문서로 표현할줄 알아야한다.
-// 가로   세로   오른쪽아래대각선   오른쪽위대각선
-// 돌을 둔 그 순간 그 돌을 기준으로 5개가 연결되는지를 체크해야한다.
+// 1. 오목판 만들기 - 2차원 배열
+// 2. 가운데 커서 두기
+// 3. 커서 이동
+// 4. 커서가 맵 밖으로 탈출하는거 막기
+// 5. 흑돌 백돌
+// 6. 흑돌 백돌 위치 중복 방지
+// 7. 룰 정리
+//    - 가로 5개, 세로 5개, 대각선 5개
